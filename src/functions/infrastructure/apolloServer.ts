b03e342c480c7d1a0b01d8c6d52209cb86c87ed2
Takuya Miyata <takumiya081@gmail.com>
2020-06-07 22:17:02 +0900

    response log

--- src/functions/infrastructure/apolloServer.ts
+++ src/functions/infrastructure/apolloServer.ts
@@ -3,4 +3,25 @@ import depthLimit from 'graphql-depth-limit';
 
 import {schema} from './schema';
 
-export const apolloServer = new ApolloServer({schema, validationRules: [depthLimit(4)]});
+export const apolloServer = new ApolloServer({
+  schema,
+  validationRules: [depthLimit(4)],
+  plugins: [
+    {
+      requestDidStart: ({request}) => {
+        // eslint-disable-next-line no-console
+        console.info('graphql server request start', {request});
+        return {
+          didEncounterErrors: ({errors}) => {
+            // eslint-disable-next-line no-console
+            console.warn('graphql server error', {errors});
+          },
+          willSendResponse: ({response}) => {
+            // eslint-disable-next-line no-console
+            console.info('graphql server response', {response});
+          },
+        };
+      },
+    },
+  ],
+});
