3492e1e4b771eb85bdca4397cce6078a5c6b084f
Takuya Miyata <takumiya081@gmail.com>
2020-06-08 00:51:30 +0900

    jestの設定

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+const {pathsToModuleNameMapper} = require('ts-jest/utils');
+const {compilerOptions} = require('./tsconfig');
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: false,
+  roots: ['<rootDir>/src'],
+  errorOnDeprecated: true,
+  globals: {
+    'ts-jest': {
+
+    },
+  },
+  verbose: true,
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {prefix: '<rootDir>/src/'}),
+};
