ef204329026069063795171ded612a63c726447e
Takuya Miyata <takumiya081@gmail.com>
2020-06-07 22:15:47 +0900

    depthLimit

--- src/functions/infrastructure/apolloServer.ts
+++ src/functions/infrastructure/apolloServer.ts
@@ -1,5 +1,6 @@
 import {ApolloServer} from 'apollo-server-micro';
+import depthLimit from 'graphql-depth-limit';
 
 import {schema} from './schema';
 
-export const apolloServer = new ApolloServer({schema});
+export const apolloServer = new ApolloServer({schema, validationRules: [depthLimit(4)]});
