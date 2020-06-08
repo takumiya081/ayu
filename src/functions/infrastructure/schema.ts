6d6605aefeafa385ee4da8976d911b9a62afe94e
Takuya Miyata <takumiya081@gmail.com>
2020-06-08 15:30:35 +0900

    schema に resolver読み込ませる

--- src/functions/infrastructure/schema.ts
+++ src/functions/infrastructure/schema.ts
@@ -1,10 +1,14 @@
-import {makeExecutableSchema} from 'apollo-server-micro';
+import {IResolvers, makeExecutableSchema} from 'apollo-server-micro';
+
+import {resolvers} from '@/functions/interface/resolver/resolvers';
 
 import typeDefs from './typeDef';
 
 export const schema = makeExecutableSchema({
   typeDefs,
-  // TODO: resolvers,
+  // apolloとgraphql-code-genのtypeが微妙にマッチしないので強制的に合わせる
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  resolvers: resolvers as IResolvers<{}>,
   resolverValidationOptions: {
     requireResolversForResolveType: false,
   },
